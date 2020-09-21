94317fd736512bc144e130066034f05d02c23677
Thieu Nguyen <thieu@bluegrassdigital.com>
2020-09-21 17:47:20 +0700

    Show images in detail

--- src/containers/CaseStudyDetail/index.js
+++ src/containers/CaseStudyDetail/index.js
@@ -7,8 +7,8 @@ const CaseStudyDetail = () => {
   const [title1, setTitle1] = useState('');
   const [title2, setTitle2] = useState('');
   const [content, setContent] = useState('');
-
   const [title3, setTitle3] = useState('');
+  const [images, setImages] = useState([]);
 
   useEffect(() => {
     console.log("CaseStudyDetail");
@@ -25,8 +25,10 @@ const CaseStudyDetail = () => {
     setTitle2(data[1].title);
     setContent(data[1].content);
     setTitle3(data[2].title);
+    setImages(data[3].images);
   }
 
+  const baseUrl = 'https://raw.githubusercontent.com/thieumao/ShowCaseNodeJS/master/';
   return (
     <div>
       <h1>Case Study Detail {projectID}</h1>
@@ -34,6 +36,11 @@ const CaseStudyDetail = () => {
       <h3>{title2}</h3>
       <h4>{content}</h4>
       <h3>{title3}</h3>
+      <img src={baseUrl + images[0]} alt={title1} width="100" height="100" />
+      <img src={baseUrl + images[1]} alt={title1} width="100" height="100" />
+      <img src={baseUrl + images[2]} alt={title1} width="100" height="100" />
+      <img src={baseUrl + images[3]} alt={title1} width="100" height="100" />
+      <img src={baseUrl + images[4]} alt={title1} width="100" height="100" />
     </div>
   );
 }
