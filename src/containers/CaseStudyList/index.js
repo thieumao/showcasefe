044167f219a88fff28dfa3a7a6bf01828ffd1d33
Thieu Nguyen <thieu@bluegrassdigital.com>
2020-09-21 15:06:49 +0700

    Show Project List

--- src/containers/CaseStudyList/index.js
+++ src/containers/CaseStudyList/index.js
@@ -1,42 +1,45 @@
-import React, { useEffect } from 'react';
-import projectAPI from '../../api/projectAPI'
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import projectAPI from "../../api/projectAPI";
+import Project from './shared/Project';
 
 const CaseStudyList = () => {
+  const [projectList, setProjectList] = useState([]);
 
   useEffect(() => {
-    console.log('Thieu Mao');
+    console.log("Thieu Mao");
     try {
-      projectAPI.getAll().then(data => {
+      projectAPI.getAll().then((data) => {
         console.log(data);
+        const caseStudies = data && data.caseStudies ? data.caseStudies : [];
+        setProjectList(caseStudies);
       });
-      /// cs_episerver0.json
+      // / cs_episerver0.json
       // projectAPI.get('cs_episerver0').then(data => {
       //   console.log(data);
       // });
-    } catch (error) {
-
-    }
+    } catch (error) {}
   }, []);
 
+  const itemClick = (item, index) => {
+    console.log(index);
+    console.log(item);
+  }
+
+  // group_mask: 2
+  // id: "cs_other1"
+  // img: "http://ncs.niteco.se/boncom/ipad-image.png"
+  // link: "http://ncs.niteco.se/boncom/"
+  // name: "boncom"
+  // tags: ["boncom"]
+  const listItems = projectList.map((item, index) => (
+    <Project project={item} onPress={() => itemClick(item, index)} />
+  ));
   return (
     <div id="container">
       <h1>Case Study List</h1>
-      <NumberList />
+      <ul className="project-list">{listItems}</ul>
     </div>
   );
-}
+};
 
 export default CaseStudyList;
-
-
-function NumberList() {
-  const listItems = [1, 2, 3, 4, 5, 6, 7].map((number) =>
-    <li key={number.toString()}>
-      {number}
-    </li>
-  );
-  return (
-    <ul>{listItems}</ul>
-  );
-}
