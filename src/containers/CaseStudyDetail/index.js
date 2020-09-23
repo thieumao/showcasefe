5fa5db97fa6be3e90e8c4234ddfc6bed66581ef0
Thieu Nguyen <thieu@bluegrassdigital.com>
2020-09-23 16:49:02 +0700

    Update UI

--- src/containers/CaseStudyDetail/index.js
+++ src/containers/CaseStudyDetail/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState }  from 'react';
 import { useParams } from "react-router-dom";
 import projectAPI from "../../api/projectAPI";
+import "./styles.scss";
 
 const CaseStudyDetail = () => {
   let { projectID } = useParams();
@@ -42,15 +43,15 @@ const CaseStudyDetail = () => {
 
   const baseUrl = 'https://raw.githubusercontent.com/thieumao/ShowCaseNodeJS/master/';
   return (
-    <div>
-      <h1>Case Study Detail {projectID}</h1>
-      <img src={baseUrl + images[2]} alt={title1} width="400" height="100" />
+    <div className="detail-container">
+      {/* <h1>Case Study Detail {projectID}</h1> */}
+      <img className="img1" src={baseUrl + images[2]} alt={title1} width="100%" height="1%" />
       <h3>{title1}</h3>
       {list1Items}
-      <img src={baseUrl + images[3]} alt={title1} width="400" height="400" />
+      <img className="img2" src={baseUrl + images[3]} alt={title1} width="50%" height="50%" />
       <h3>{title2}</h3>
       <div>{content}</div>
-      <img src={baseUrl + images[4]} alt={title1} width="400" height="400" />
+      <img className="img3" src={baseUrl + images[4]} alt={title1} width="50%" height="50%" />
       <h3>{title3}</h3>
       {list3Items}
       {/* <img src={baseUrl + images[0]} alt={title1} width="100" height="100" />
